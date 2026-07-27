3b0e3c706d4a1ceaf01ea6ab20a891cbed51fb71
Mr Paulin <killianwork381@gmail.com>
2026-07-27 21:38:48 +0300

    Update post_generator.py

--- post_generator.py
+++ post_generator.py
@@ -3,9 +3,9 @@ from analysis_generator import generate_analysis
 
 def generate_public_post(news):
     return (
-        f"<b>{news['title']}</b>\n\n"
+        f"🔥 <b>{news['title']}</b>\n\n"
         f"{news['description']}\n\n"
-        "#Crypto"
+        "#Crypto #Новости"
     )
 
 
@@ -13,8 +13,9 @@ def generate_private_post(news):
     analysis = generate_analysis(news)
 
     return (
-        f"<b>{news['title']}</b>\n\n"
+        f"🔥 <b>{news['title']}</b>\n\n"
         f"{news['description']}\n\n"
         f"{analysis}\n\n"
-        "⚠️ Не является финансовой рекомендацией."
+        "⚠️ Не является финансовой рекомендацией.\n\n"
+        "#Crypto #Аналитика"
     )
