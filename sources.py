9e70bd74ec3b4f3973f14ca6a7a38e2ab913e6cf
Mr Paulin <killianwork381@gmail.com>
2026-07-27 21:58:17 +0300

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,6 +1,7 @@
 import feedparser
 from translator import translate
 from cleaner import clean_text
+from datetime import datetime, timezone
 
 
 COINDESK_RSS = "https://www.coindesk.com/arc/outboundfeeds/rss/"
@@ -16,11 +17,32 @@ def get_latest_news():
         feed = feedparser.parse(url)
 
         for entry in feed.entries[:5]:
+
+            published = getattr(entry, "published_parsed", None)
+        
+            if published:
+                published_time = datetime(
+                    *published[:6],
+                    tzinfo=timezone.utc
+                )
+        
+                age = datetime.now(
+                    timezone.utc
+                ) - published_time
+        
+                if age.total_seconds() > 86400:
+                    continue
+        
             news.append({
-                "title": clean_text(translate(entry.title)),
-
-                "description": clean_text(translate(getattr(entry, "summary", ""))),
-})
-
+                "title": clean_text(
+                    translate(entry.title)
+                ),
+                "description": clean_text(
+                    translate(
+                        getattr(entry, "summary", "")
+                    )
+                ),
+                "link": entry.link
+            })
     return news
     
