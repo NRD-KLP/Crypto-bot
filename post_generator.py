935102d6308a00ac2fdd56fcaf142075af432c9c
Mr Paulin <killianwork381@gmail.com>
2026-07-27 21:32:33 +0300

    Update post_generator.py

--- post_generator.py
+++ post_generator.py
@@ -13,7 +13,6 @@ def generate_private_post(news):
     analysis = generate_analysis(news)
 
     return (
-        "🔒 <b>Bit Ref 4U | Аналитика</b>\n\n"
         f"<b>{news['title']}</b>\n\n"
         f"{news['description']}\n\n"
         f"{analysis}\n\n"
