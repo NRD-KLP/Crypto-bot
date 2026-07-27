b58ac240a192972e3276000e77b4c86c1388259b
Mr Paulin <killianwork381@gmail.com>
2026-07-27 21:34:35 +0300

    Create cleaner.py

---
+++ cleaner.py
@@ -0,0 +1,20 @@
+import re
+
+
+def clean_text(text):
+    if not text:
+        return ""
+
+    text = re.sub(
+        r"<.*?>",
+        "",
+        text
+    )
+
+    text = re.sub(
+        r"\s+",
+        " ",
+        text
+    )
+
+    return text.strip()
