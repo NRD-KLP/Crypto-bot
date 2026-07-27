baa45edd7acc5639d8e42bd2a1b1316119b480a0
Mr Paulin <killianwork381@gmail.com>
2026-07-27 20:10:06 +0300

    Update post_generator.py

--- post_generator.py
+++ post_generator.py
@@ -1,14 +1,13 @@
-def generate_public_post(news, market):
+def generate_public_post(news):
     return (
-        "📰 <b>Крипто-новость</b>\n\n"
         f"<b>{news['title']}</b>\n\n"
-        f"😨 Fear & Greed: {market['value']} ({market['classification']})\n\n"
+        f"{news['description']}\n\n"
     )
 
 
-def generate_private_post(news, market):
+def generate_private_post(news):
     return (
         "🔒 <b>Bit Ref 4U | Аналитика</b>\n\n"
         f"<b>{news['title']}</b>\n\n"
-        f"😨 Индекс страха и жадности: {market['value']} ({market['classification']})\n\n"
+        f"{news['description']}\n\n"
     )
