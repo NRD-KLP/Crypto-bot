30fe996ab169af3de25016c267d2ecd500465e31
Mr Paulin <killianwork381@gmail.com>
2026-07-27 20:07:12 +0300

    Update post_generator.py

--- post_generator.py
+++ post_generator.py
@@ -10,8 +10,5 @@ def generate_private_post(news, market):
     return (
         "🔒 <b>Bit Ref 4U | Аналитика</b>\n\n"
         f"<b>{news['title']}</b>\n\n"
-        "📊 <b>Рынок сейчас</b>\n"
-        f"💰 BTC: ${market['btc_price']} ({market['btc_change']}%)\n"
-        f"💎 ETH: ${market['eth_price']} ({market['eth_change']}%)\n\n"
         f"😨 Индекс страха и жадности: {market['value']} ({market['classification']})\n\n"
     )
