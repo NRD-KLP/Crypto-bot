0638856b5878d67a8f8015ea400a3565da7edeee
Mr Paulin <killianwork381@gmail.com>
2026-07-28 11:32:02 +0300

    Update database.py

--- database.py
+++ database.py
@@ -165,8 +165,9 @@ async def is_text_published(text):
             """,
             (text_hash,)
         )
-    result = await cursor.fetchone()
-    await cursor.close()
+
+        result = await cursor.fetchone()
+        await cursor.close()
 
         return result is not None
 
