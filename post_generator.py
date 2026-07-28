c36dd93aed93c76321e8ef2aaeec816dc5f307c9
Mr Paulin <killianwork381@gmail.com>
2026-07-28 06:01:28 +0300

    Update post_generator.py

--- post_generator.py
+++ post_generator.py
@@ -1,14 +1,5 @@
 from analysis_generator import generate_analysis
 
-
-def generate_public_post(news):
-    return (
-        f"🔥 <b>{news['title']}</b>\n\n"
-        f"{news['description']}\n\n"
-        "#Crypto #Новости"
-    )
-
-
 def generate_private_post(news):
     analysis = generate_analysis(news)
 
