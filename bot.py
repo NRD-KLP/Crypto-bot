dba6a829c9ee08467a0370e544e9ee5a6919680e
Mr Paulin <killianwork381@gmail.com>
2026-07-28 08:24:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ def main():
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("buy", buy))
-    application.add_handler(
+    app.add_handler(
     MessageHandler(
         filters.PHOTO,
         get_photo_id
