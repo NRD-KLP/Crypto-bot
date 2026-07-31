7a795fa8c6a90ee3326723b7656cc200b25285d2
Mr Paulin <killianwork381@gmail.com>
2026-07-31 08:28:27 +0300

    Update content_manager.py

--- content_manager.py
+++ content_manager.py
@@ -1,6 +1,9 @@
-import traceback
 import asyncio
-from database import (is_news_published, save_published_news,)
+
+from database import (
+    is_news_published,
+    save_published_news,
+)
 
 from config import (
     CHANNEL_ID,
@@ -8,40 +11,91 @@ from config import (
 )
 
 from sources import get_latest_news
-from post_generator import generate_private_post
-from image_manager import get_random_image
+
+from post_generator import (
+    generate_private_post,
+)
+
+from image_manager import (
+    get_random_image,
+)
+
 
 
 async def content_manager(app):
+
+    print(
+        "Content manager started"
+    )
+
+
     while True:
+
         try:
+
             news_list = get_latest_news()
+
 
             for news in news_list:
 
-                if await is_news_published(news["link"], "private"):
+
+                if await is_news_published(
+                    news["link"],
+                    "private"
+                ):
                     continue
 
-                print("Trying private post:", news["title"])
 
-                text = generate_private_post(news)
-                print("Private generated:", news["title"])
+
+                print(
+                    "New post:",
+                    news["title"]
+                )
+
+
+
+                text = generate_private_post(
+                    news
+                )
+
+
+
+                photo = get_random_image()
+
+
 
                 await app.bot.send_photo(
                     chat_id=CHANNEL_ID,
-                    photo=get_random_image(),
+                    photo=photo,
                     caption=text,
                     parse_mode="HTML"
                 )
 
-                await save_published_news(news["link"], "private")
 
-                print("Private post sent.")
+
+                await save_published_news(
+                    news["link"],
+                    "private"
+                )
+
+
+                print(
+                    "Private post sent"
+                )
+
 
                 break
 
-        except Exception as e:
-            print(f"Private manager error:")
-            traceback.print_exc()
 
-        await asyncio.sleep(PRIVATE_POST_INTERVAL)
+
+        except Exception as e:
+
+            print(
+                f"Content manager error: {e}"
+            )
+
+
+
+        await asyncio.sleep(
+            PRIVATE_POST_INTERVAL
+        )
