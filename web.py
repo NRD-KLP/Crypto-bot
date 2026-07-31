9019051dd5dc1cc9ac06a7689bd924a0230c3d28
Mr Paulin <killianwork381@gmail.com>
2026-07-31 08:38:18 +0300

    Update web.py

--- web.py
+++ web.py
@@ -1,14 +1,21 @@
 from flask import Flask
-import os
+
+
 
 app = Flask(__name__)
 
-@app.get("/")
+
+
+@app.route("/")
 def home():
+
     return "Bit Ref 4U Bot is running!"
 
+
+
 def run_web():
+
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
+        port=8080
     )
