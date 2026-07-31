527a4640db01fbe75a8f28d4c77dabd2a4b4e705
Mr Paulin <killianwork381@gmail.com>
2026-07-31 08:29:30 +0300

    Update post_generator.py

--- post_generator.py
+++ post_generator.py
@@ -1,7 +1,36 @@
 from summarizer import summarize
 
 
-def generate_private_post(news):
+def escape_html(text: str):
+
     return (
-        f"{summarize(news)}\n\n"
+        text
+        .replace("&", "&amp;")
+        .replace("<", "&lt;")
+        .replace(">", "&gt;")
+    )
+
+
+
+def generate_private_post(news):
+
+    title = escape_html(
+        news.get(
+            "title",
+            "Без заголовка"
+        )
+    )
+
+
+    description = escape_html(
+        summarize(
+            news
+        )
+    )
+
+
+    return (
+        f"<b>{title}</b>\n\n"
+        f"{description}\n\n"
+        "💎 <i>Bit Ref 4U Premium</i>"
     )
