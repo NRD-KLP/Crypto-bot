8494552b2850f4b32aa73ebb7661c11cb5285f17
Mr Paulin <killianwork381@gmail.com>
2026-07-31 08:35:40 +0300

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,9 +1,36 @@
 import re
 
 
-def clean_text(text):
+
+BAD_WORDS = [
+
+    "Read more",
+
+    "Continue reading",
+
+    "Continue Reading",
+
+    "Read More",
+
+    "Learn more",
+
+    "Click here",
+
+    "Subscribe",
+
+]
+
+
+
+def clean_text(text: str):
+
     if not text:
+
         return ""
+
+
+
+    # Удаляем HTML
 
     text = re.sub(
         r"<.*?>",
@@ -11,10 +38,27 @@ def clean_text(text):
         text
     )
 
+
+
+    # Удаляем мусорные фразы
+
+    for word in BAD_WORDS:
+
+        text = text.replace(
+            word,
+            ""
+        )
+
+
+
+    # Убираем лишние пробелы
+
     text = re.sub(
         r"\s+",
         " ",
         text
     )
+
+
 
     return text.strip()
